c7ca4bc2230ab6096baa0e48aeb71fc9687aab24
Henrique-Palharim <henrique.dornelas@unesp.br>
2026-04-20 17:01:45 -0300

    alterações visuais na crediária aterro

--- crediaria-aterro/resources/views/view_tonelagem.blade.php
+++ crediaria-aterro/resources/views/view_tonelagem.blade.php
@@ -1,37 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário</title>
+@extends('layout.site')
 
-    <style>
-        input {
-            display: block;
-            margin-bottom: 20px;
-        }
-    </style>
-
-</head>
-<body>
+@section('conteudo')
     
     <form action="calc_tonelagem" method="post">
 
-    @csrf
-    <h2>Insira os dados solicitados</h2>
+        @csrf
+        <h2>Insira os Dados</h2>
 
-    <label for="pesoBruto">Peso Bruto: </label>
-    <input type="number" name="pesoBruto" id="pesoBruto">
+        <label for="pesoBruto">Peso Bruto: </label>
+        <input type="number" name="pesoBruto" id="pesoBruto">
 
-    <label for="tara">Tara: </label>
-    <input type="number" name="tara" id="tara">
+        <label for="tara">Tara: </label>
+        <input type="number" name="tara" id="tara">
 
-    <label for="valorTonelada">Valor da Tonelada: </label>
-    <input step="0.01" type="number" name="valorTonelada" id="valorTonelada">
+        <label for="valorTonelada">Valor da Tonelada: </label>
+        <input step="0.01" type="number" name="valorTonelada" id="valorTonelada">
 
-    <input type="submit" value="Calcular">
+        <input type="submit" value="Calcular">
 
-</form>
+    </form>
 
-</body>
-</html>
+@endsection
